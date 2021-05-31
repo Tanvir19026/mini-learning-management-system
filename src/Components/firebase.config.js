8d1e15c72f17d00ec23a2d1e88d55a0511c8d6dc
Tanvir19026 <tanvirrafi1999@gmail.com>
2021-05-31 12:13:14 +0600

    start commit

---
+++ src/Components/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC5drl5xJIwXKVlCx5uyvvdpV9b-r4Jz9I",
+    authDomain: "mini-learning-website.firebaseapp.com",
+    projectId: "mini-learning-website",
+    storageBucket: "mini-learning-website.appspot.com",
+    messagingSenderId: "526140087065",
+    appId: "1:526140087065:web:e43c52b74cd55da4690388"
+  };
+  export default firebaseConfig;
